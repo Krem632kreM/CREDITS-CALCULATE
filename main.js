892f738b9af85ec6c3a8b00065d27aa1d2ef09f0
Krem632kreM <krempinatatyana@gmail.com>
2022-01-04 02:54:31 +0400

    урок 8,9/ C УСЛОЖНЕНЫМИ ЗАДАНИЯМИ

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let money,
     isNumber = function (n) {
       return !isNaN(parseFloat(n)) && isFinite(n)
     }
+    
 
 start();
 
@@ -18,10 +19,25 @@ let appData = {
   expenses: {},
   addExpenses: [],
   deposit: false,
+  percentDeposit: 0,
+  moneyDeposit: 0,
   mission: 50000,
   period: 3,
   asking: function () {
-    let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", 'ком,инт');
+    if(confirm("Есть ли у Вас дополнительный заработок?")) {
+      let itemIncome
+      do {
+      itemIncome = prompt("Какой у вас есть дополнительный заработок?", "Таксую")
+      } while(isNumber(itemIncome))
+      let cashIncome
+      do {
+      cashIncome = prompt("Сколько в месяц зарабатываете на этом?", 10000)
+      }while (!isNumber(cashIncome));
+      appData.income[itemIncome]=cashIncome
+    }
+    let addExpenses
+    do { addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", 'ком,инт');
+    } while (isNumber(addExpenses))
     appData.addExpenses = addExpenses.toLowerCase().split(',');
     appData.deposit = confirm("Есть ли у вас депозит в банке?");
     let j;
@@ -69,17 +85,59 @@ let appData = {
     } else {
       return ("Что то пошло не так")
     }
+  },
+  getInfoDeposit: function(){
+    if(appData.deposit){
+      do {
+      appData.percentDeposit=prompt('какой годовой процент?', 10)
+      } while(!isNumber(appData.percentDeposit))
+      do {
+      appData.moneyDeposit=prompt('какая сумма заложена?', 10000)
+    } while(!isNumber(appData.moneyDeposit))
+    }
+  },
+  calcSevedMoney: function(){
+    return appData.budgetMonth*appData.period
   }
 
 }
 
-appData.asking();
+/*appData.asking();
 appData.getBudget();
+appData.getInfoDeposit();*/
 
 console.log('Расходы за месяц: ' + appData.getExpensesMonth())
 console.log(appData.getTargetMonth());
 console.log(appData.getStatusIncome());
 
-for (const key in appData) {
-  console.log('Наша программа включает в себя данные: ' + key + ' ' + appData[key])
-}
+const buttonStart = document.getElementById('start')
+const buttonPlusAdditionalIncome = document.getElementsByTagName('button')[0]
+const buttonPlusObligatoryExpenses = document.getElementsByTagName('button')[1]
+const depositCheck = document.querySelector('#deposit-check')
+const additionalIncomeItem_0 = document.querySelectorAll('.additional_income-item')[0]
+const additionalIncomeItem_1 = document.querySelectorAll('.additional_income-item')[1]
+/*
+Каждый элемент в правой части программы через класс(не через querySelector), которые имеют в имени класса "-value", начиная с class="budget_day-value" и заканчивая class="target_month-value">*/
+ const budgetMonthValue = document.getElementsByClassName('budget_month-value') , 
+ budgetDayValue = document.getElementsByClassName(' budget_day-value'),
+ expensesMonthValue = document.getElementsByClassName('expenses_month-value'), 
+ additionalIncomeValue  = document.getElementsByClassName('additional_income-value'),
+ additionalExpensesValue = document.getElementsByClassName('additional_expenses-value'), 
+ incomePeriodValue = document.getElementsByClassName('income_period-value'), 
+ targetMonthValue = document.getElementsByClassName('target_month-value');
+
+/*Оставшиеся поля через querySelector каждый в отдельную переменную:
+
+поля ввода (input) с левой стороны и не забудьте про range.
+*/
+console.log(budgetDayValue)
+const salaryAmount = document.querySelector('.salary-amount'),
+incomeTitle = document.querySelector('.income-title'),
+incomeAmount = document.querySelector('.income-amount'),
+expensesTitle = document.querySelector('.expenses-title'),
+expensesAmount = document.querySelector('.expenses-amount'),
+additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+targetAmount = document.querySelector('.target-amount'),
+Range = document.querySelector('[type="range"]');
+
+console.log(Range)
--- second.js
+++ second.js
@@ -141,7 +141,7 @@ function test() {
   console.log(arguments.shift())
 }
 
-test(1,2,3,4)*/
+test(1,2,3,4)
 
 let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресение']
 var date = new Date();
@@ -160,6 +160,170 @@ div.innerHTML = html;
 document.body.appendChild(div);
 
   }
+*/
+
+const people = [
+  {name: 'Владилен', age: 25, budget: 4000},
+  {name: 'Елена', age: 17, budget: 3400},
+  {name: 'Игорь', age: 49, budget: 50000},
+  {name: 'Михаил', age: 15, budget: 1800},
+  {name: 'Василиса', age: 24, budget: 25000},
+  {name: 'Виктория', age: 38, budget: 2300}
+]
+
+/*for(let i=0; i<people.length; i++){
+  console.log(people[i])
+}
+
+for (const person of people) {
+  console.log(person)
+}*/
+
+//ForEach
+/*people.forEach(function (person){
+  console.log(person)
+  })
+people.forEach(person => console.log(person))*/
+//Map
+/*
+const newPeople = people.map(person => `${person.name[0].toUpperCase()}${person.name.substr(1)}`).join(", ")
+
+const newPeople2 = people.map(person => person.name.replace(/./, m => m[0].toUpperCase())).join(", ")
+
+console.log(newPeople)
+console.log(newPeople2)
+
+People = people.map(person => person.age * 3)
+console.log(People)
+*/
+//Filter
+/*const adults=[]
+for(let i=0; i<people.length;i++){
+if(people[i].age>=18) adults.push(people[i])
+}
+console.log(adults)*/
+
+/*const adults  = people.filter(person => {
+  if (person.age>=18) return true
+})
+console.log(adults)*/
+
+/*const adults  = people.filter(person => person.age>=18)
+console.log(adults)*/
+
+//Reduce//получаем финальное значение совершив итерации по акому то массиву
+/*
+let amount=0;
+for(let i=0; i<people.length;i++){
+  amount+=people[i].budget
+}
+console.log(amount)*/
+/*
+const amount = people.reduce((total, person)=> total+person.budget, 0)
+
+console.log(amount)*/
+//Find
+
+/*const Igor = people.find(person =>person.name==="Игорь")
+console.log(Igor)
+//FindIndex
+const IgorIndex = people.findIndex(person =>person.name==="Игорь")
+console.log(IgorIndex)*/
+/*
+const amount = people
+.filter(person=>person.budget>3000)
+.map(person => {
+  return {
+    info: `${person.name}(${person.age})`,
+    budget: Math.sqrt(person.budget)
+  }
+})
+.reduce((total,person)=>total+person.budget, 0)
+
+console.log(amount)
+
+console.log(appData.addExpenses.map(n => n.replace(/./, m => m.toUpperCase())).join(', '))
+
+const mainHead = document.querySelector('.btn_plus');
+const computedStyleMainHead = getComputedStyle(mainHead);
+console.log(computedStyleMainHead.backgroundOrigin);
+console.log(computedStyleMainHead);*/
+
+
+
+let func = function () {
+  let date = new Date()
+let days = [
+  'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота'
+];
+let n = date.getDay();
+
+function getLocalDayYearMonth_2digit(d) {
+  let month = date.toLocaleDateString("ru-RU", { month: '2-digit' })
+  let year = date.toLocaleDateString("ru-RU", { year: '2-digit' })
+
+  return d.toLocaleDateString("ru-RU", { day: '2-digit' }) + "." + month + "." + year;
+}
+
+function getLocalTime_2digit(d) {
+  return d.toLocaleTimeString("ru-RU", { 
+    hours: '2-digit',
+    minutes: '2-digit',
+    seconds: '2-digit'
+  });
+}
+
+function getLocalTime(d) {
+  let hoursWord = function (hours) {
+    if (hours===1||hours===21) return "час"
+    else if (hours>=2&&hours<5&&hours>=12&&hours<15&&hours>=22&&hours<24) return "часа"
+    else return "часов" 
+  }
+
+  let minutesWord = function (minutes) {
+    if (minutes%10===1&&minutes!==11) return "минута"
+    else if (minutes%10===2||minutes%10===3||minutes%10===4) return "минуты"
+    else if(minutes===11) return "минут" 
+    else return "минут" 
+  }
+
+  let secondsWord = function (seconds) {
+    if (seconds%10===1&&seconds!==11) return "секунда"
+    else if ((seconds%10===2||seconds%10===3||seconds%10===4)&&seconds!==12&&seconds!==13&&seconds!==14) return "секунды"
+    else if (seconds===11&&seconds!==12&&seconds!==13&&seconds!==14) return "секунд" 
+    else return "секунд" 
+  }
+
+  return d.getHours() + " " + hoursWord(d.getHours()) + " " + d.getMinutes() + " " + minutesWord(d.getMinutes()) + " " + d.getSeconds()+ " " + secondsWord(d.getSeconds());
+}
+
+function getLocalDayYearMonth(d) {
+  let word = date.toLocaleDateString("ru-RU", { month: 'long' })
+  let month = word.substring(0, word.length-1) + "я"
+  let year = date.toLocaleDateString("ru-RU", { year: 'numeric' }) + ' года'
+
+  return d.toLocaleDateString("ru-RU", { day: 'numeric' }) + " " + month + " " + year;
+}
+let str = `${"Сегодня "+ days[n] +', '+ getLocalDayYearMonth(date) +', '+getLocalTime(date)  + '<BR/>' + getLocalDayYearMonth_2digit(date) + ' - ' + getLocalTime_2digit(date)}`;
+const div = document.createElement('div');
+//div.innerHTML = str
+document.body.innerHTML = str;
+}
+setInterval(func,1000);
+
+
+
+
+
+
+
+
 
 
 
